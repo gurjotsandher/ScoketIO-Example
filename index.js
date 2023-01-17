647527003ab67c826742dcab9e47fb7f53f79d9d
Gurjot Sandher <gsa143@sfu.ca>
2023-01-16 21:49:40 -0800

    added comments/notes

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ io.on('connection', (socket) => {
     // that ID can be obtained socket.id
     
     socket.on('nickname', (nickname) => {
+
         console.log(`a user named ${nickname} connected`)
         this.nickname = nickname
         
@@ -31,7 +32,12 @@ io.on('connection', (socket) => {
 
         // TODO: Why is IO used here instead of socket?
         // Look at the info.txt file.
-        io.emit('online users', online_users) 
+
+        // This emits to everyone INCLUDING the sender
+        io.emit('online users', online_users)
+
+        // This won't update the online users for the older users
+        // socket.emit('online users', online_users)
 
         // that socket can broadcast and emit a listening event
         // that broadcasts to the other sockets
@@ -42,6 +48,7 @@ io.on('connection', (socket) => {
     // We use broadcast because the user who is typing doesn't
     // need to see that he/she is typing. 
     socket.on('isTyping', (user) => {
+
         if(user.isTyping){
             socket.broadcast.emit("isTyping", `${user.nickname} is typing...`)
             console.log(`${user.nickname} is typing...`)
@@ -52,11 +59,18 @@ io.on('connection', (socket) => {
     // By using broadcast we send it to all of the other users 
     // except for the sender
     socket.on('chat message', (message) => {
+
         console.log(`message: ${message}`)
         socket.broadcast.emit('chat message', message)
     })
 
+    // We check to see which user is leaving
+    // We can use our online_users map (object in JS)
+    // online_users key is their nickname and the value is the socket.id
+    // We can delete the user by using their nickname and then reemit the new
+    // list (in this case an object because of socketIO
     socket.on('disconnect', (reason) => {
+
         console.log('disconnecting user: ' + online_users[socket.nickname])
         delete online_users[socket.nickname]
         console.log(online_users)
